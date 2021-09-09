03633ae57aa4def104d442f795b0c8afd07a25db
batuanr <88823692+batuanr@users.noreply.github.com>
2021-09-09 07:23:08 +0700

    fix

--- game.js
+++ game.js
@@ -2,20 +2,21 @@ let canvas = document.getElementById("game");
 let ctx = canvas.getContext("2d");
 
 let birdimg = new Image();
-let tubeUp = new Image();
-let tubeDown = new Image();
+let pipeTop = new Image();
+let pipeBot = new Image();
     birdimg.src = "bird.png";
-    tubeUp.src = 'ongtren.png';
-    tubeDown.src = "ongduoi.png";
-let betweenTube = 150;
-let tubeLong = tubeUp.height + betweenTube;
+    pipeTop.src = 'ongtren.png';
+    pipeBot.src = "ongduoi.png";
+let betweenPipe = 150;
+let pipeDown = pipeTop.height + betweenPipe;
 let score = 0;
+let dy = 1;
 let bird={
     x:canvas.width/4,
     y:canvas.height/2
 }
-let tube = [];
-tube[0] = {
+let pipe = [];
+pipe[0] = {
     x: canvas.width,
     y:0
 }
@@ -24,27 +25,28 @@ tube[0] = {
 function start() {
     ctx.clearRect(0,0,700,500)
     ctx.drawImage(birdimg,bird.x,bird.y);
-    bird.y+=3
+    bird.y+=dy;
+    dy *= 1.04
 
     ctx.fillText('Score: '+score,5,20)
-    for (let i = 0; i < tube.length; i++) {
-        ctx.drawImage(tubeUp,tube[i].x,tube[i].y);
-        ctx.drawImage(tubeDown,tube[i].x,tube[i].y + tubeLong);
-        tube[i].x -=5;
-        if (tube[i].x === canvas.width/2){
-            tube.push({
+    for (let i = 0; i < pipe.length; i++) {
+        ctx.drawImage(pipeTop,pipe[i].x,pipe[i].y);
+        ctx.drawImage(pipeBot,pipe[i].x,pipe[i].y + pipeDown);
+        pipe[i].x -=5;
+        if (pipe[i].x === canvas.width/2){
+            pipe.push({
                 x: canvas.width,
-                y: Math.floor(Math.random()*tubeUp.height) - tubeUp.height
+                y: Math.floor(Math.random()*pipeTop.height) - pipeTop.height
             })
         }
-        if (tube[i].x === 0){
-            tube.splice(0,1);
+        if (pipe[i].x === 0){
+            pipe.splice(0,1);
         }
-        if (tube[i].x === bird.x) score++;
+        if (pipe[i].x === bird.x) score++;
         if (bird.y + birdimg.height > canvas.height - 20 ||
-        bird.x + birdimg.width >= tube[i].x && bird.x <= tube[i].x + tubeUp.width
-            && (bird.y <= tube[i].y + tubeUp.height ||
-                bird.y + birdimg.height >= tube[i].y + tubeDown.height)){
+        bird.x + birdimg.width >= pipe[i].x && bird.x <= pipe[i].x + pipeTop.width
+            && (bird.y < pipe[i].y + pipeTop.height ||
+                bird.y + birdimg.height > pipe[i].y + pipeBot.height)){
             return;
         }
     }
@@ -52,7 +54,8 @@ function start() {
     requestAnimationFrame(start)
 
 }
-document.addEventListener("keydown", function (){
-    bird.y-=30
+document.addEventListener("keydown",  ()=>{
+    bird.y-=40;
+    dy = 1;
 })
 start()
