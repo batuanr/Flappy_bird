696e934ee7b79e7cafa625ab4f16d8a9bf1e7a07
batuanr <88823692+batuanr@users.noreply.github.com>
2021-09-09 16:45:33 +0700

    xong

--- game.js
+++ game.js
@@ -1,76 +1,124 @@
+// cho canvas vào
 let canvas = document.getElementById("game");
 let ctx = canvas.getContext("2d");
 
-let birdimg = new Image();
+//khởi tạo hình ảnh và các biến
+let birdimg1 = new Image();
+let end = new Image();
+let end2 = new Image();
+let disPlayImg = new Image()
 let pipeTop = new Image();
 let pipeBot = new Image();
-    birdimg.src = "bird.png";
+
+    birdimg1.src = "bird1.png";
+    end.src = "end.png";
+    end2.src = "reset.png";
+    disPlayImg.src = 'nen2.png'
     pipeTop.src = 'ongtren.png';
     pipeBot.src = "ongduoi.png";
+
+let maxScore = localStorage.getItem('score');
 let betweenPipe = 150;
 let pipeDown = pipeTop.height + betweenPipe;
 let score = 0;
-let maxScore = localStorage.getItem('score');
+
 let dy = 1;
+let dx = 5;
+// tạo đối tượng bird
 let bird={
     x:canvas.width/4,
     y:canvas.height/2
 }
+// tạo đối tượng pipe
 let pipe = [];
 pipe[0] = {
     x: canvas.width,
     y:0
 }
-
-
-function start() {
-    ctx.clearRect(0,0,700,500)
-    ctx.drawImage(birdimg,bird.x,bird.y);
+ // bắt đầu tạo disPlay
+function disPlay() {
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    ctx.drawImage(disPlayImg,0,0)
+    ctx.drawImage(birdimg1,bird.x,bird.y);
     bird.y+=dy;
     dy *= 1.04
 
-    ctx.font = '16px Arial'
+    ctx.font = '16px'
     ctx.fillStyle = 'red'
     ctx.fillText('Score: '+score,5,20)
     ctx.fillText('Max Score: '+ maxScore,580,20)
     for (let i = 0; i < pipe.length; i++) {
         ctx.drawImage(pipeTop,pipe[i].x,pipe[i].y);
         ctx.drawImage(pipeBot,pipe[i].x,pipe[i].y + pipeDown);
-        pipe[i].x -=5;
+        pipe[i].x -= dx;
         if (pipe[i].x === canvas.width/2){
             pipe.push({
                 x: canvas.width,
                 y: Math.floor(Math.random()*pipeTop.height) - pipeTop.height
             })
         }
-        if (pipe[i].x === 0){
+        if (pipe[i].x + pipeTop.width === 0){
             pipe.splice(0,1);
         }
         if (pipe[i].x === bird.x){
             score++;
-            localStorage.setItem('score',score)
+
             if (score > maxScore){
                 maxScore = score;
+                localStorage.setItem('score',score);
             }
         }
-        if (bird.y + birdimg.height > canvas.height - 20 ||
-        bird.x + birdimg.width >= pipe[i].x && bird.x <= pipe[i].x + pipeTop.width
+        if (bird.y + birdimg1.height > canvas.height - 20 ||
+        bird.x + birdimg1.width >= pipe[i].x && bird.x <= pipe[i].x + pipeTop.width
             && (bird.y < pipe[i].y + pipeTop.height ||
-                bird.y + birdimg.height > pipe[i].y + pipeDown )){
-            return;
+                bird.y + birdimg1.height > pipe[i].y + pipeDown )){
+            return  (
+                ctx.drawImage(end,250,100),
+                    ctx.drawImage(end2,290,270),
+                    score=0
+
+
+            );
+
         }
     }
 
-    requestAnimationFrame(start)
-    canvas.addEventListener("click", ()=>{
-        start()
-    })
+    requestAnimationFrame(disPlay)
+
 
 }
+// cho bird nhảy lên
 document.addEventListener("keydown",  ()=>{
     bird.y-=40;
     dy = 1;
 })
-canvas.addEventListener("click", ()=>{
-    start()
-})
+// hàm lấy tọa độ xy của chuột
+function printMousePos(e) {
+    let cursorX = e.pageX;
+    let cursorY = e.pageY;
+    if (cursorX>= 300 && cursorX <= 345 && cursorY >= 280 && cursorY <= 345 && score === 0){
+        start()
+    }
+}
+// và gọi hàm
+document.addEventListener("click", printMousePos)
+
+
+function start() {
+     pipeDown = pipeTop.height + betweenPipe;
+     score = 0;
+
+    dy = 1;
+    dx = 5;
+     bird={
+        x:canvas.width/4,
+        y:canvas.height/2
+    }
+     pipe = [];
+    pipe[0] = {
+        x: canvas.width,
+        y:0
+    }
+
+    disPlay()
+}
